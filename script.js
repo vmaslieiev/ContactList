22ca96bda304e13693a930e36176d4e6846d6edd
Vadym Maslieiev <macleev93@gmail.com>
2019-10-05 15:03:42 +0300

    js was changed

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ const inputLastName = document.getElementById('input-last');
 const inputNumber = document.getElementById('input-number');
 const button = document.getElementById('btn');
 
-let  inputFirstValue = null;
+let inputFirstValue = null;
 let inputLastValue = null;
 let inputNumberValue = null;
 
 button.addEventListener('click', addContact);
 
-function addContact(e){
+function addContact(e) {
   e.preventDefault();
   getInputValue();
   checkInputs();
@@ -21,71 +21,68 @@ function addContact(e){
   clearInput();
 }
 
-
-
-function getInputValue(){
+function getInputValue() {
   inputFirstValue = inputFirstName.value;
   inputLastValue = inputLastName.value;
   inputNumberValue = inputNumber.value;
 }
 
-function checkInputs(){
-  if(inputFirstValue.length === 0){
+function checkInputs() {
+  if (inputFirstValue.length === 0) {
     alert('Please, Insert First Name');
     return false;
   }
 
-  if(inputLastValue.length === 0){
+  if (inputLastValue.length === 0) {
     alert('Please, Insert Last Name');
     return false;
   }
 
-  if(inputNumberValue.length === 0 || isNaN(inputNumberValue)){
+  if (inputNumberValue.length === 0 || isNaN(inputNumberValue)) {
     alert('Please, Insert Phone Number');
     return false;
   }
 }
 
-function addInfo(){
-  if(checkInputs() == false){
+function addInfo() {
+  if (checkInputs() == false) {
     return;
-  }else{
+  } else {
     const newTr = document.createElement('tr');
-  
+
     const newTdFirstName = document.createElement('td');
     const newTdLastName = document.createElement('td');
     const newTdNumber = document.createElement('td');
     const newTdDelete = document.createElement('td');
     const newTdDeleteText = document.createTextNode('X');
-  
+
     newTr.classList.add('newTr');
     newTdDelete.classList.add('delete-btn');
-  
+
     newTdFirstName.append(inputFirstValue);
     newTdLastName.append(inputLastValue);
     newTdNumber.append(inputNumberValue);
     newTdDelete.append(newTdDeleteText);
-  
+
     newTr.append(newTdFirstName);
     newTr.append(newTdLastName);
     newTr.append(newTdNumber);
     newTr.append(newTdDelete);
-  
+
     newList.append(newTr);
 
     newTr.addEventListener('click', deleteElement);
+
+    function deleteElement() {
+      newTr.remove()
+    }
   }
 }
 
-function clearInput(){
+function clearInput() {
   inputFirstName.value = "";
   inputLastName.value = "";
   inputNumber.value = "";
 }
 
-function deleteElement(event){
-  if(event.target.classList.contains('newTr')){
-    parentElement.remove();
-    console.log(event);
-  }
-}
+
